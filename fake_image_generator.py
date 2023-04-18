9a1968df028648ae73af3745fce5fc08e17e97b7
Roshan Chokshi <chokshiroshan@gmail.com>
2023-04-18 03:34:03 -0700

    Create blob function implemented

--- fake_image_generator.py
+++ fake_image_generator.py
@@ -1,12 +1,30 @@
-# generate 1000x1000 white pixel image
+import random
+from PIL import Image, ImageDraw
 
-from PIL import Image
+
+def create_base_image(resolution=(1000000, 1000000)):
+    img = Image.new('RGB', (resolution), color='white')
+    return img
 
 
-def create_base_image():
-    img = Image.new('RGB', (1000, 1000), color='white')
-    img.save('base_image.png')
+def save_image(img, filename):
+    img.save(filename)
+
+
+def create_circle_blob(img):
+    draw = ImageDraw.Draw(img)
+    area_of_image = img.size[0] * img.size[1]
+    area_of_blob = random.randint(0.25 * area_of_image, 0.35 * area_of_image)
+    print("blob covers: ", area_of_blob/area_of_image, "%")
+    radius = int((area_of_blob / 3.14) ** 0.5)
+    x = random.randint(radius, img.size[0] - radius)
+    y = random.randint(radius, img.size[1] - radius)
+    draw.ellipse((x - radius, y - radius, x +
+                 radius, y + radius), fill='black')
+    return img
 
 
 if __name__ == "__main__":
-    create_base_image()
+    img = create_base_image((1000, 1000))
+    img = create_circle_blob(img)
+    save_image(img, 'test.png')
